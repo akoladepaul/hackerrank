ace287bf7be37e9040d32258094f36f00e44fb83
lidashuang <ldshuang@gmail.com>
2017-03-27 15:51:12 +0800

    https://www.hackerrank.com/challenges/arrays-ds

---
+++ arrays-ds.php
@@ -0,0 +1,12 @@
+<?php
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d",$n);
+$arr_temp = fgets($handle);
+$arr = explode(" ",$arr_temp);
+array_walk($arr,'intval');
+
+$rev_arr = array_reverse($arr);
+echo join(" ", $rev_arr)."\n";
+
+?>
