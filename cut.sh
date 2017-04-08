61aca875d333361f884db0ae8ff8fa30e1c6ab4a
lidashuang <ldshuang@gmail.com>
2017-04-08 18:50:15 +0800

    https://www.hackerrank.com/challenges/text-processing-cut-1

---
+++ cut.sh
@@ -0,0 +1,47 @@
+
+# http://www.folkstalk.com/2012/02/cut-command-in-unix-linux-examples.html
+
+
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-1
+cut -c3
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-2
+# 第二行和第七行
+cut -c2,7
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-3
+# 第二行到第七行
+cut -c2-7
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-4
+cut -c1-4
+
+# https://www.hackerrank.com/challenges/text-processing-cut-5
+# For each line in the input, print the first three fields.
+cut -f1-3
+# or
+cut -f-3
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-6
+cut -c13-
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-7
+cut -d' ' -f4
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-8
+# identify and display its first three words
+cut -d' ' -f1-3
+
+# https://www.hackerrank.com/challenges/text-processing-cut-9
+# print the fields from second fields to last field.
+cut  -f2-
+
+
